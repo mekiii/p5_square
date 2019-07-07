24abcca29b5585702065cd0a5374fd81c0997f14
Meki <you@example.com>
2019-07-07 18:15:29 +0200

    Create edgy variant

--- sketch.js
+++ sketch.js
@@ -7,9 +7,9 @@ function setup() {
   background(0,0,0,1);
   let cnv = createCanvas(windowWidth, windowHeight, WEBGL);
   // put setup code here
-  stroke(24, 202, 230, 1);
+  stroke(205, 203, 113, 1);
   strokeWeight(1);
-  fill(24, 202, 230, 0.1)
+  fill(24, 202, 230, 0.05)
 }
 
 
@@ -17,18 +17,16 @@ function setup() {
 function draw() {
   background(0,0,0,1);
   counter += 0.01;
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < 20; i++) {
     let angle = counter+i*0.3;
     beginShape();
     //---------------start
-      curveVertex(sin(angle)*edgeLength, cos(angle)*edgeLength, 0);
-      curveVertex(sin(angle)+0, cos(angle)+0, 0);
-      curveVertex(cos(angle), sin(angle)*edgeLength, 0);
-      curveVertex(cos(angle)*edgeLength, -noise(-angle)*sin(-angle)*edgeLength , 0);
-      curveVertex(sin(angle)*edgeLength, cos(angle)+0, 0);
-      curveVertex(sin(angle)+0, cos(angle)+0, 0);
-      curveVertex(sin(angle)+0, cos(angle)+0, 0);
-    endShape();
+      vertex(1*edgeLength, 0*edgeLength, 0);
+      vertex(sin(angle)+0, cos(angle)+0, 0);
+      vertex(cos(angle), sin(angle)*edgeLength, 0);
+      vertex(cos(angle)*edgeLength, -noise(-angle)*sin(-angle)*edgeLength , 0);
+      vertex(sin(angle)*edgeLength, cos(angle)+0, 0);
+    endShape(CLOSE);
   }
 
   // put drawing code here
